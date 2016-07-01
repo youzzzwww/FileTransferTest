745af8b5dfbe8be46965421421a18808d5ec5c7f
youzzzwww <youzzzwww@sina.cn>
2016-07-01 19:11:49 +0800

    add file common class

---
+++ FileTransferCommon/FileTransferCommon/FileTransmission.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace FileTransferCommon
+{
+    class FileTransmission
+    {
+    }
+    public class FileReceive
+    {
+        private static Int32 _default_port = 1230;
+        private static int _default_buffer_size = 4096;
+        private FileCommon _file_common;
+        public FileReceive(string filename)
+        {
+            _file_common = new FileCommon(filename);
+        }
+        public async void Run()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, _default_port);
+            listener.Start();
+            Console.WriteLine("File receive listening:");
+
+            while (true)
+            {
+                try
+                {
+                    TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+                    Task t = Process(tcpClient);
+                    await t;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private async Task Process(TcpClient tcpClient)
+        {
+            string clientEndPoint =
+              tcpClient.Client.RemoteEndPoint.ToString();
+            Console.WriteLine("Received connection request from "
+              + clientEndPoint);
+            try
+            {
+                byte[] dataBuffer = new byte[_default_buffer_size];
+                NetworkStream networkStream = tcpClient.GetStream();               
+                
+                while (true)
+                {
+                    int receiveSize = await networkStream.ReadAsync(dataBuffer, 0, _default_buffer_size);
+                    if (receiveSize >0)
+                    {
+                        _file_common.Write(dataBuffer, receiveSize);
+                    }
+                    else
+                        break; // Client closed connection
+                }
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (tcpClient.Connected)
+                    tcpClient.Close();
+            }
+        }
+    }
+}
---
+++ FileTransferCommon/FileTransferCommon/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTransferCommon
+{
+    public class FileCommon
+    {
+        private string _file_name;
+        private string _file_information_record;
+        public FileCommon(string filename)
+        {
+            _file_name = filename;
+            _file_information_record = _file_name + ".record";
+        }
+        public static bool FileExist(ref string file_path)
+        {
+            return File.Exists(file_path);
+        }
+        public bool Exists() { return File.Exists(_file_name); }
+        public long GetSize() { return new FileInfo(_file_name).Length; }
+    }
+    public class CommandResolve
+    {
+        public CommandResolve() { }
+        public static string Resolve(ref string input_line)
+        {
+            string[] input_array = input_line.Split();
+            string command = input_array[0];
+            if (command == "retrieve")
+            {
+                if (input_array.Length == 3)
+                    return "store " + input_array[1] + " " + input_array[2];
+                else
+                    return "error numbers of parameters in retrieve command.";
+            }
+            else if (command == "store")
+            {
+                if (input_array.Length == 3 && FileCommon.FileExist(ref input_array[1]))
+                {
+                    return "upload " + input_array[1] + " " + input_array[2]
+                        + " " + new FileCommon(input_array[1]).GetSize();
+                }
+                else
+                    return "error numbers of parameters in store command.";
+            }
+            else if (command == "upload")
+            {
+
+            }
+            else if (command == "error")
+            {
+                return null;
+            }
+            else
+                return "error invalid commandline";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
